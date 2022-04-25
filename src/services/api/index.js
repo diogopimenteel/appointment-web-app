37fffedc933a7f9b1c41b0481beffb0c9677d45f
Diogo Pimentel <diogopimenteel@gmail.com>
2022-04-24 23:27:11 -0300

    update api url

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-const { API_BASE_URL } = process.env;
-
 const api = axios.create(
   {
-    baseURL: API_BASE_URL || 'http://localhost:4000',
+    baseURL: 'https://appointment-pitang-api.herokuapp.com',
   },
 );
 
